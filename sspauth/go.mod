f335d3d810ea58808f1143497d850daaca071fe8
Малашенков Роман Эдуардович <rmalashen@mts.ru>
2026-06-27 17:38:43 +0300

    d

--- sspauth/go.mod
+++ sspauth/go.mod
@@ -3,3 +3,5 @@ module github.com/romamalax/libs/sspauth
 go 1.25.4
 
 require github.com/romamalax/libs/convert v0.1.0
+
+replace github.com/romamalax/libs/convert => ../convert
--- sspauth/ssp-auth.go
+++ sspauth/ssp-auth.go
@@ -1,8 +1,16 @@
 package sspauth
 
-import "github.com/romamalax/libs/convert"
+import (
+	"fmt"
+	"strings"
 
-const lenAuthCode = 6
+	"github.com/romamalax/libs/convert"
+)
+
+const (
+	AuthCodeLen = 6
+	authMod     = 1_000_000
+)
 
 type SSPAuth struct {
 	secretA, secretB int
@@ -12,12 +20,21 @@ func NewSSPAuth(secretA, secretB int) *SSPAuth {
 	return &SSPAuth{secretA, secretB}
 }
 
+func (a *SSPAuth) Code(sspID int) int {
+	if sspID <= 0 {
+		return 0
+	}
+	return (sspID*a.secretA + a.secretB) % authMod
+}
+
+func (a *SSPAuth) BuildReqURL(baseURL string, sspID int) string {
+	baseURL = strings.TrimRight(baseURL, "/")
+	return fmt.Sprintf("%s?sspid=%d&auth=%0*d", baseURL, sspID, AuthCodeLen, a.Code(sspID))
+}
+
 func (a *SSPAuth) Validate(sspID int, code []byte) bool {
-	if len(code) != lenAuthCode {
+	if sspID <= 0 || len(code) != AuthCodeLen {
 		return false
 	}
-	if (sspID*a.secretA+a.secretB)%1_000_000 == convert.BytesToInt(code) {
-		return true
-	}
-	return false
+	return a.Code(sspID) == convert.BytesToInt(code)
 }
